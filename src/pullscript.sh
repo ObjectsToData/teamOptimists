9d2a2b89255f845cb24da959d91e704b03a0d2a1
Zaturrby <rjkorteschiel@gmail.com>
2014-10-01 11:17:06 +0200

    first pull script

--- src/pullscript.sh
+++ src/pullscript.sh
@@ -3,29 +3,38 @@ baseurl="http://api.nytimes.com/svc/search/v2/articlesearch.json"
 
 # Current key = Robert-Jan
 key="60b4b3ab2322ca40d4cb413c571d5c98:3:69789701"
-query="jpg"
+query="Kenya"
 page=0
 
 # construct the complete request
 requestUrl="$baseurl?q=$query&api-key=$key&page=$page"
 
 # get first results page and extract total number of results/hits
-hits=`curl -s $requestUrl | jq '.response.meta.hits'`
+hits=`curl $requestUrl | jq '.response.meta.hits'`
 
 # The NY Times API returns 10 results per request
 # The number of hits divided by 10 gives the number
 # of requests we have to make to get all results
-totalRequests=hits/10
+page=0
+totalcount=0
 
 # Iteratively download all the requests
-for ((page=0; page<$totalRequests; page++))
+while [ $totalcount -lt 100 ]
 do
+
 	# construct request URL while iterating over paging
 	requestUrl="$baseurl?q=$query&api-key=$key&page=$page"
 
 	# get results and extract individual article metadata
 	# from the response JSON
-	curl -s $requestUrl | jq -c '.response.docs[]'
+
+	curl $requestUrl > temp.json
+	jq -c ".response.docs[].multimedia" temp.json > tempMulti.json
+	cat tempMulti.json | grep ".jpg" > tempMultiSeperated.json
+	count=`wc -l tempMultiSeperated.json | awk '{print $1}'`
+
+	let totalcount=$totalcount+$count
+	let page=$page+1
 
 	# wait 0.2 seconds between request to not overload the server
 	sleep 0.2
