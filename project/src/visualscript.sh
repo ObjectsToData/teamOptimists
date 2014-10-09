a3055e31a3687059ac9e8eba2c69bead3e9e6122
Zaturrby <rjkorteschiel@gmail.com>
2014-10-09 21:40:56 +0200

    added a visual task,, to copy the files to the right dirs, at least on my system

---
+++ project/src/visualscript.sh
@@ -0,0 +1,8 @@
+rm -rf ../../../teamOptimists-Viz/src/images/static01.nyt.com
+cp -R static01.nyt.com ../../../teamOptimists-Viz/src/images/static01.nyt.com
+
+rm -rf ../../../teamOptimists-Viz/src/data
+cp -R countryData ../../../teamOptimists-Viz/src/data
+
+cd ../../../teamOptimists-Viz/
+grunt
